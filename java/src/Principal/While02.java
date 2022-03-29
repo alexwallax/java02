57473f40fa17cafcf9c748238e2932dbc20540bd
Alex Wallace <alexwallax@gmail.com>
2022-03-29 09:55:52 -0300

    While continuação

---
+++ java/src/Principal/While02.java
@@ -0,0 +1,22 @@
+package Principal;
+
+import java.util.Scanner;
+
+public class While02 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+			String valor = "";
+			
+			while(!valor.equalsIgnoreCase("sair")) {
+				System.out.print("Digite algo: ");
+				valor = entrada.nextLine();
+			}
+		 
+		entrada.close();
+
+	}
+ 
+}
