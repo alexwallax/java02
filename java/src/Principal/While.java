94341dc06f886179e24a2f8dc298e4857071d194
Alex Wallace <alexwallax@gmail.com>
2022-03-29 09:33:14 -0300

    While

---
+++ java/src/Principal/While.java
@@ -0,0 +1,15 @@
+package Principal;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		int contador = 1;
+		
+		while(contador <=10 ) {
+			System.out.printf("i = %d\n", contador);
+			contador++;
+		}
+	} 
+
+}
