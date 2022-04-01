e56fd7393b53ccf4ee574e0f6458243b3690cc99
Alex Wallace <alexwallax@gmail.com>
2022-04-01 16:47:26 -0300

    Switch 02

---
+++ java/src/Principal/Switch2.java
@@ -0,0 +1,50 @@
+package Principal;
+
+import java.util.Scanner;
+
+public class Switch2 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String conceito = "";
+		System.out.print("Informe a nota:");
+		int nota = entrada.nextInt();
+		
+		switch (nota) {
+		
+		case 10: case 9: 
+			conceito = "A";
+			break; 
+			
+		case 8: case 7: { //obs pode colocar blocos de codigos
+			conceito = "B";
+			break; 
+		}
+		case 6:
+		case 5: 
+			conceito = "C";
+			break;  
+			
+		case 4: case 3: 
+			conceito = "D";
+			break; 
+			
+		case 2: case 1: case 0: 
+			conceito = "E";
+			break; 
+			
+		default:
+			System.out.println("Conceito não informado!");
+		}
+		
+		System.out.println("Conceito é " + conceito);
+		
+		entrada.close();	
+			
+
+
+	}
+
+}
