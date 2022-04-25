993c200e8a958108e728da8e9f4feeaa06af0b58
Alex Wallace <alexwallax@gmail.com>
2022-04-25 09:18:35 -0300

    Construtores

--- java/src/classe02/Data.java
+++ java/src/classe02/Data.java
@@ -6,5 +6,21 @@ public class Data {
 	int mes;
 	int ano;
 	
+	Data() { 
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	
+	Data(int diaInicial, int mesInicial, int anoInicial) {
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
+	String obterData() {
+		return String.format("%d/%d/%d\n\n", dia, mes, ano);
+	}
+	
 	
 }
--- java/src/classe02/DataTeste.java
+++ java/src/classe02/DataTeste.java
@@ -4,22 +4,31 @@ public class DataTeste {
 	
 	public static void main(String[] args) {
 		
-		Data d1 = new Data();
-		Data d2 = new Data();
+		Data d1 = new Data(); // vai buscar os valores do construtor padrão 
+		Data d2 = new Data(31 ,12, 2022); // não precisa mais setar os valores la embaixo, coloca como parametros.
 		
-		d1.dia = 13;
-		d1.mes = 12;
-		d1.ano = 1985;
+		//d1.dia = 13;
+		//d1.mes = 12; 
+		//d1.ano = 1985;
 		
-		d2.dia = 27;
-		d2.mes = 9;
-		d2.ano = 1999;
+		//d2.dia = 27; 
+		//d2.mes = 9;
+		//d2.ano = 1999;
 		
-		System.out.println(d1.dia + "/" + d1.mes + "/" + d1.ano); 
+
+			
+		//System.out.println(d1.dia + "/" + d1.mes + "/" + d1.ano + "\n"); 
 		
-		System.out.println(d2.dia + "/" + d2.mes + "/" + d2.ano);
+		//System.out.printf("%d/%d/%d\n\n", d1.dia, d1.mes, d1.ano);
 		
+		//System.out.println(d2.dia + "/" + d2.mes + "/" + d2.ano); 
 		
+		String dataFormatada1 = d1.obterData();
+		
+		System.out.println(dataFormatada1);
+		System.out.printf(d2.obterData());
+		
+		 
 	}
 
 }
